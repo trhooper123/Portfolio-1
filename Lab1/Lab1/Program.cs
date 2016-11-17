08b6a8cb81d31a529384bf9d6c3fa010f817ed5b
trhooper123 <Taylor@Taylors-MacBook-Air.local>
2016-11-17 17:15:36 -0500

    Initial Commit

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Taylor Hooper
+//Lab 1
+//Due: 9/8/2013
+//CIS 199-01
+// This program displays on separate lines, your name, your birthday, your hobbies, your favorite book, and your favorite movie.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Taylor Hooper");
+            Console.WriteLine("My birthday is February 25th");
+            Console.WriteLine("My hobbies are playing Xbox, Building Computers, and working at my electronic repair business");
+            Console.WriteLine("My favorite book is The Shawshank Redemption");
+            Console.WriteLine("My favorite movie is GoodFellas");
+
+        }
+    }
+}
